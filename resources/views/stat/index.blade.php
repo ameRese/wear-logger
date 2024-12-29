352823381375d8af5d5c2dae4bfe5f6927d6dcbb
ameRese <yarukix@gmail.com>
2024-12-29 22:20:45 +0900

    feat: 統計情報インデックス画面作成

---
+++ resources/views/stat/index.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            統計情報
+        </h2>
+    </x-slot>
+    <ul class="max-w-7xl mx-auto px-6">
+        <li>
+            <a href="#">未使用アイテムリスト</a>
+        </li>
+        <li>
+            <a href="#">着用回数ランキング</a>
+        </li>
+    </ul>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StatController;
 use App\Http\Controllers\WearLogController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,8 @@ Route::resource('item', ItemController::class);
 
 Route::post('wear_log/store/{item}', [WearLogController::class, 'store'])->name('wear_log.store');
 Route::delete('wear_log/{wear_log}', [WearLogController::class, 'destroy'])->name('wear_log.destroy');
+
+Route::get('stat', [StatController::class, 'index'])->name('stat.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
