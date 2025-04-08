03f74740cffdfc67750d158f3c7913ea81549c5c
ameRese <yarukix@gmail.com>
2025-04-08 22:17:55 +0900

    feat: フォント見直し

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['"Noto Sans JP"', 'Helvetica', 'arial', 'sans-serif'],
             },
         },
     },
