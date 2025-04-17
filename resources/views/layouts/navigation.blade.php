95e40aa9d3fdd5d0520bdbacfd37033971164765
ameRese <yarukix@gmail.com>
2025-04-17 12:31:04 +0900

    fix: ナビゲーションメニューのレスポンシブ部を微修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100 fixed w-full z-50">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
@@ -11,7 +11,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-8 md:ms-10 sm:flex">
                     <x-nav-link :href="route('item.index')" :active="request()->routeIs('item.index')">
                         アイテム一覧・着用登録
                     </x-nav-link>
